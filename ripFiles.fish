1d163a8d5b054dc63ddd084a0f62c5d418dcb97c
MotorTruck1221 <motortruck1221@protonmail.com>
2024-09-17 05:54:33 -0600

    Every. Single. Version of Subway surfers one could imagine

---
+++ ripFiles.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+
+# Curl the files FIRST! xargs -n 1 curl -O < urls.txt (you may need to pass along custom headers, copy as curl from the network tab is you friend here)
+
+for file in (cat full.txt)
+    set source_dir "~/code/ruby-assets/subway-surfers/html5"
+    set folder_path (dirname "$file")
+    set file_name (basename "$file")
+    echo "Moving $file_name to $folder_path/$file_name"
+    mkdir -p "$folder_path/"
+    mv "$file_name" "$folder_path/"
+end
